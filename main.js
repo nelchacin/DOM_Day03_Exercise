2491c6e020b89a85dad86d6df8ec2d5c786084e3
nelchacin <nelchacindev@gmail.com>
2022-11-18 11:03:55 +0100

    3 commit

--- main.js
+++ main.js
@@ -7,39 +7,70 @@ const inputPriceCar = document.querySelector("#priceCar")
 const inputPriceTotal = document.querySelector("#priceTotal")
 
 
-inputCalculatePrice.onclick = () => {
-    let inputNumberofNightsValue = (inputNumberofNights.value) * 1
-    let inputCitySelectionValue = inputCitySelection.value
-}
+// inputCalculatePrice.onclick = () => {
+//     let inputNumberofNightsValue = (inputNumberofNights.value) * 1
+//     let inputCitySelectionValue = inputCitySelection.value
+// }
 
 
-let totalHotel = 0
-
-function calcularCoste() { }
+// function calcularCoste() { }
 
 function costeHotel(num) {
-    totalHotel = inputNumberofNights * 140
+    let totalHotel = num * 140
+    return totalHotel
 }
-function costeAvion(city, num) {
+function costeAvion(num) {
     let oviedoCity = 15
     let tokyoCity = 700
     let madridCity = 90
     let barcelonaCity = 90
-    if (city.value === "Oviedo" && num >= 3) {
-        return oviedoCity * 0.1
-    } else if (city.value === "Tokyo" && num >= 3) {
-        return tokyoCity * 0.1
-    } else if (city.value === "Madrid" && num >= 3) {
-        return madridCity * 0.1
-    } else if (city.value === "Barcelona" && num >= 3) {
-        return barcelonaCity * 0.1
-    }
-}
+    if (inputCitySelection.value === "Oviedo") {
+        if (num > 3) {
+            let discount = oviedoCity * 0.9
+            return discount
+        } else {
+            return oviedoCity
+        }
 
-function CosteAlquilerCoche(num) {
-    if (inputNumberofNights === 3 && inputNumberofNights < 7) {
-        return totalHotel - 20
-    } else if (inputNumberofNights > 7) {
-        return totalHotel - 50
+    } else if (inputCitySelection.value === "Tokyo") {
+        if (num > 3) {
+            let discount = tokyoCity * 0.9
+            return discount
+        } else {
+            return tokyoCity
+        }
+
+    } else if (inputCitySelection.value === "Madrid") {
+        if (num > 3) {
+            let discount = madridCity * 0.9
+            return discount
+        } else {
+            return madridCity
+        }
+    } else if (inputCitySelection.value === "Barcelona") {
+        if (num > 3) {
+            let discount = barcelonaCity * 0.9
+            return discount
+        } else {
+            return barcelonaCity
+        }
     }
+
+    function CosteAlquilerCoche(num) {
+        let totalCar = num * 40
+        if (num < 3) {
+            return totalCar
+        } else if (num > 2 && num < 7) {
+            return totalCar - 20
+        } else {
+            return totalCar - 50
+        }
+    }
+
+    inputCalculatePrice.onclick = () => {
+        inputPriceCar.value = CosteAlquilerCoche(inputNumberofNights.value)
+        inputPriceTicket.value = costeAvion(inputNumberofNights.value)
+        inputPriceHotel.value = costeHotel(inputNumberofNights)
+    }
+    inputPriceTotal.value = (inputPriceHotel.value * 1) + (inputPriceCar.value * 1) + (inputPriceTicket.value * 1)
 }
